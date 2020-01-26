e80a4699d39474d22f1c5a7d542dfe9cc3b96932
koyo-1008 <kou1088@yahoo.co.jp>
2020-01-26 15:16:02 +0900

    自動更新終了

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,64 +1,100 @@
 $(function(){
+  
   function buildHTML(message){
     if ( message.image) {
     var html = `
-      <div class="main-group">
-      <div class="main-group_name">
-      ${message.user_name}
-      </div>
-      <div class="main-group_message">
-      ${message.created_at}
-      </div>
-      </div>
-      <div class="main-massage">
-      <p class="lower-message__content">
-      ${message.content}
-      </p>
-      <img src=${message.image} >
-    </div>`
+      <div class="message_main" data-message-id = ${message.id}>
+        <div class="main-group">
+        <div class="main-group_name">
+        ${message.user_name}
+        </div>
+        <div class="main-group_message">
+        ${message.created_at}
+        </div>
+        </div>
+        <div class="main-massage">
+        <p class="lower-message__content">
+        ${message.content}
+        </p>
+        <img src=${message.image} >
+        </div>
+      </div>`
     return html;
     } else {
       var html = `
-      <div class="main-group">
-      <div class="main-group_name">
-      ${message.user_name}
-      </div>
-      <div class="main-group_message">
-      ${message.created_at}
-      </div>
-      </div>
-      <div class="main-massage">
-      <p class="lower-message__content">
-      ${message.content}
-      </p>
+      <div class="message_main" data-message-id = ${message.id}>
+        <div class="main-group">
+        <div class="main-group_name">
+        ${message.user_name}
+        </div>
+        <div class="main-group_message">
+        ${message.created_at}
+        </div>
+        </div>
+        <div class="main-massage">
+        <p class="lower-message__content">
+        ${message.content}
+        </p>
+        </div>
       </div>`
       return html;
     };
   }
-$('#new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
-  })
-  .done(function(data){
-    var html = buildHTML(data);
-    console.log(html)
-    
-    $('.main').append(html);  
-    $('form')[0].reset();
-    $('.main').animate({ scrollTop: $('.main')[0].scrollHeight});    
-    $('.footer_send_btn').prop('disabled', false);
-  })
-  .fail(function() {
-  alert("メッセージ送信に失敗しました");
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      
+      $('.main').append(html);  
+      $('form')[0].reset();
+      $('.main').animate({ scrollTop: $('.main')[0].scrollHeight});    
+      $('.footer_send_btn').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
     });
   })
-});
+
+    var reloadMessages = function() {
+      last_message_id = $('.message_main:last').data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+          //追加するHTMLの入れ物を作る
+          var insertHTML = '';
+          //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+          $.each(messages, function(i, message) {
+            insertHTML += buildHTML(message)
+          });
+          //メッセージが入ったHTMLに、入れ物ごと追加
+          $('.main').append(insertHTML);
+          $(".main").animate({ scrollTop: $('.main')[0].scrollHeight});
+          $("#new_message")[0].reset();
+          $(".form__submit").prop("disabled", false);
+        }
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました。');
+      });
+    };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+      setInterval(reloadMessages, 7000);
+    }
+})
+
   
